6adbc0d2701f167909b999f9016a6d4b8c6aa15d
TOMLIN <TOMLIN@LAPTOP-067IT145>
2017-02-20 20:14:02 -0800

    <MS 2/20> SoleTrader & RedBus

---
+++ src/test/java/testscripts/SoleTrader_DataProvider.java
@@ -0,0 +1,84 @@
+ package testscripts;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import dataprovider.ApplicationData;
+import factory.BrowserFactory;
+import factory.DataProviderFactory;
+import pages.BusinessDetailsPg;
+import pages.ImportantInfoPg;
+import utility.TestBase;
+
+public class SoleTrader_DataProvider extends TestBase
+{
+	
+	@Test(priority = 1, dataProviderClass = ApplicationData.class, dataProvider = "IntroPg")
+	public void IntroPg(String StText_Line1, String StText_Line2, String StText_Line3, String StText_Line4, String StText_Line5, String StText_Line6, String StText_Line7, String StText_Line8, String StText_Line9, String StText_Line10, String StText_Line11)
+	{
+		test = report.startTest("SoleTrader DP -"+TestBase.getCurrentTimestamp());
+		
+		BrowserFactory.driver.get(DataProviderFactory.getconfig().getAppUrl());
+		
+		ImportantInfoPg Impinfo = PageFactory.initElements(BrowserFactory.driver, ImportantInfoPg.class);
+		test.log(LogStatus.INFO, "Intro PageFactory Initialization");
+		
+		DataProviderFactory.getbase().WaitTillPageLoads("DDA_FORM");
+		
+		VerifyHeaderObjects();
+		Assert.assertEquals(BrowserFactory.driver.getTitle(), Impinfo.PAGE_TITLE);
+		test.log(LogStatus.INFO, "Application Launched; Header Images and Important Info PageTitle Verification - Success");
+		
+		Assert.assertEquals(Impinfo.StText_Line1.getText(), StText_Line1);
+		Assert.assertEquals(Impinfo.StText_Line2.getText(), StText_Line2);
+		Assert.assertEquals(Impinfo.StText_Line3.getText(), StText_Line3);
+		Assert.assertEquals(Impinfo.StText_Line4.getText(), StText_Line4);
+		Assert.assertEquals(Impinfo.StText_Line5.getText(), StText_Line5);
+		Assert.assertEquals(Impinfo.StText_Line6.getText(), StText_Line6);
+		Assert.assertEquals(Impinfo.StText_Line7.getText(), StText_Line7);
+		Assert.assertEquals(Impinfo.StText_Line8.getText(), StText_Line8);
+		Assert.assertEquals(Impinfo.StText_Line9.getText(), StText_Line9);
+		Assert.assertEquals(Impinfo.StText_Line10.getText(), StText_Line10);
+		Assert.assertEquals(Impinfo.StText_Line11.getText(), StText_Line11);
+		
+		test.log(LogStatus.INFO, "Static Text Validation - Success");
+		
+		//EOPScreenshot(BrowserFactory.driver.getTitle());
+		ClickElement(Impinfo.Btn_Next);
+		
+		test.log(LogStatus.PASS, "IntroPg - Success");
+	}
+	
+	@Test(priority = 2, dataProviderClass = ApplicationData.class, dataProvider = "BusinessDetailsPg")
+	public void BD(String Txt_BusiName, String Txt_BusiTelephoneNumber, String Txt_BusiEmail, String Txt_MobileNumber, String Txt_BuildingNumber, String Txt_Street, String Txt_County, String Txt_Town, String Txt_PostCode, String Txt_TradingName, String Dropdown_stat_cor_add)
+	{
+		BusinessDetailsPg bd = PageFactory.initElements(BrowserFactory.driver, BusinessDetailsPg.class);
+		test.log(LogStatus.INFO, "Business Details PageFactory Initialization");
+		
+		DataProviderFactory.getbase().WaitTillPageLoads("Busname");
+		
+		VerifyHeaderObjects();
+		Assert.assertEquals(BrowserFactory.driver.getTitle(), bd.PAGE_TITLE);
+		test.log(LogStatus.INFO, "Header Images and Business Details PageTitle Verification - Success");
+		
+		InputText(bd.Txt_BusiName, Txt_BusiName);
+		InputText(bd.Txt_BusiTelephoneNumber, Txt_BusiTelephoneNumber);
+		InputText(bd.Txt_BusiEmail, Txt_BusiEmail);
+		InputText(bd.Txt_MobileNumber, Txt_MobileNumber);
+		InputText(bd.Txt_BuildingNumber, Txt_BuildingNumber);
+		InputText(bd.Txt_Street, Txt_Street);
+		InputText(bd.Txt_Town, Txt_Town);
+		InputText(bd.Txt_County, Txt_County);
+		InputText(bd.Txt_PostCode, Txt_PostCode);
+		InputText(bd.Txt_TradingName, Txt_TradingName);
+		ClickElement(bd.Radio_TradingName);
+		DropDownSelection(bd.Dropdown_stat_cor_add, Dropdown_stat_cor_add);
+		
+		//EOPScreenshot(BrowserFactory.driver.getTitle());
+		
+		test.log(LogStatus.PASS, "Business Details data input - Success");
+	}
+}
---
+++ src/test/java/utility/TestBase.java
@@ -0,0 +1,113 @@
+package utility;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import factory.BrowserFactory;
+import factory.DataProviderFactory;
+import pages.Header;
+
+public class TestBase
+{
+	public static ExtentTest test;
+	public static ExtentReports report;
+	//WebDriver driver;
+	
+	@Parameters("browser")
+	@BeforeTest
+	public void Setup(String browser)
+	{
+		BrowserFactory.driver = BrowserFactory.getBrowser(browser);
+		report = new ExtentReports(DataProviderFactory.getconfig().getReportsPath(), false);
+	}
+	
+	@AfterTest
+	public void Teardown()
+	{
+		BrowserFactory.driver.quit();
+		report.endTest(test);
+		report.flush();
+	}
+	
+	//Method to Verify Header
+	public static void VerifyHeaderObjects()
+	{
+		Header header = PageFactory.initElements(BrowserFactory.driver, Header.class);
+		header.H_currentSection.isDisplayed();
+		header.H_PgHeader.isDisplayed();
+		header.H_ProgIndicator.isDisplayed();
+		header.H_RBSlogo.isDisplayed();
+	}
+	
+	//Failed Screenshot
+	@AfterMethod
+	public void FailedScreenshot(ITestResult result)
+	{
+		if (result.getStatus()==ITestResult.FAILURE)
+		{
+			String Screenshotpath = DataProviderFactory.getscreenshot().CaptureScreenshot(BrowserFactory.driver, result.getName());
+			test.log(LogStatus.FAIL, test.addScreenCapture(Screenshotpath));
+		}
+
+	}
+	
+	//End of Page Screenshot
+	public static void EOPScreenshot(String Pgtitlename)
+	{
+		test.log(LogStatus.PASS, test.addScreenCapture(DataProviderFactory.getscreenshot().CaptureScreenshot(BrowserFactory.driver, BrowserFactory.driver.getTitle())));
+	}
+	
+	//Current Date and Timestamp
+	public static String getCurrentTimestamp()
+	{
+		String timestamp = new SimpleDateFormat("yyy_MM_dd-HH.mm.ss").format(new Date());
+		return timestamp;
+	}
+	
+	//Explicit Wait until a WebElement is displayed in a page
+	public WebElement WaitTillPageLoads(String elementid)
+	{
+		WebDriverWait wait = new WebDriverWait(BrowserFactory.driver, 10);
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elementid)));
+		return element;
+	}
+	
+	//WebElement Actions - Click
+	public static void ClickElement(WebElement element)
+	{
+		element.isEnabled();
+		element.click();
+	}
+	
+	//WebElement Actions - Input Text
+	public static void InputText(WebElement element, String input)
+	{
+		element.sendKeys(input);
+		Assert.assertEquals(element.getAttribute("value"), input);
+	}
+	
+	//WebElement Actions - Dropdown selection
+	public static void DropDownSelection(WebElement element, String dropdowntext)
+	{
+		Select dropdown = new Select(element);
+		dropdown.selectByVisibleText(dropdowntext);
+	}
+
+}
